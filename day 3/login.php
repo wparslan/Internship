00b3eb976a7f120572a0aeca46f8844004e4d744
Zain-wpbrigade <121797419+Zain-wpbrigade@users.noreply.github.com>
2023-01-14 12:16:14 +0500

    Update login.php

--- day 3/login.php
+++ day 3/login.php
@@ -1,21 +1,62 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>WpBrigade login Page</title>
-</head>
-<body>
-    <center>   <!-- centre the page  -->
-    <form action="welcome.php" method="post">   <!-- action is conect the files "login" and wlecome page each other and post, when we get data from user this variable will hide your data on url bar and access data from other page-->
-        <h1>WPBrigade Login</h1>
-       <h3>Username <input type="text" name="email"></h3> <!-- take input as a text -->
-       <h3>Password <input type="password" name="pwd"></h3> <!-- take input as a password -->
-       <h2> <input type="submit" name="login" value="Login"></h2>
-    </form>
-    </center>
-</body>
-</html> 
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>login form</title>
+  </head>
+  <body>
+  <div class="container"> <!-- class container is integrite with bootstrap link -->
+  <h1>Login Form</h1>
+  <hr>
+  </div>
+    <div class="container">
+        <form  action="login.php" method="post">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="text" class="form-control" name="email_id" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" name="password" id="exampleInputPassword1" placeholder="Password">
+            </div>
+           
+            <button type="submit" class="btn btn-primary" name="btnclick" >Login!</button>
+        </form>
+
+    </div>
+  </body>
+</html>
+
+<!------------------------php code ------------------------>
+
+<?php
+
+session_start();
+
+if(isset($_POST["btnclick"])){
+// set the variable for email and password 
+    $day3_email_id = $_POST["email_id"];
+    $day3_password = $_POST["password"];
+//false the session coz we check the email and password is correct or not in the next condition 
+    $_SESSION["status"]=false;
+
+    //condition for checking valid input from user
+
+    if ( $day3_email_id == "abc@gmail.com" && $day3_password == "1234" ){
+
+        $_SESSION["email_id"] = $email_id;
+        $_SESSION["status"]= true;
+        header("Location: welcome.php");  // link the welcome page after login with correct email and password
+    }
+    else{// if password or email are wrong the message will pop up and page will go to login page
+        echo "<script>alert('username or password incorrect!')</script>";
+        echo "<script>location.href='login.php'</script>";
+    }   
+}
+?>
